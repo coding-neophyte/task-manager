82d01e4149a1141fa6e8a948319d1066a0002213
Julius Franklin <jjfranklin88@outlook.com>
2022-12-19 13:22:59 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const app = require('./lib/app');
+const pool = require('./lib/utils/pool');
+const Mongoose = require('mongoose');
+
+const API_URL = process.env.API_URL || 'http://localhost';
+const PORT = process.env.PORT || 7890;
+
+Mongoose.set('strictQuery', true);
+
+Mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.mq2yh.mongodb.net/?retryWrites=true&w=majority`)
+  .then(() => console.log('database connected'))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`🚀  Server started on ${API_URL}:${PORT}`);
+});
+
+process.on('exit', () => {
+  console.log('👋  Goodbye!');
+  pool.end();
+});
